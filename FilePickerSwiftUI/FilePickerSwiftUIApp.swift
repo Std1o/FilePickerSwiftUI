cd75d375ac115a80b32e1f700b29e34ddcda1f65
Std1o <37378410+Std1o@users.noreply.github.com>
2021-05-06 12:32:46 +0300

    Initial commit

---
+++ FilePickerSwiftUI/FilePickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilePickerSwiftUIApp.swift
+//  FilePickerSwiftUI
+//
+//  Created by Илья on 06.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct FilePickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
